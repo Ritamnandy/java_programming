69de99a9a0502b6abc9f5782d6409ff8c2b79a8d
Ritamnandy <nandyritam519@gmail.com>
2026-04-22 12:50:37 +0530

    2 program done

--- Day_01/ConvertInchestoMeters.java
+++ Day_01/ConvertInchestoMeters.java
@@ -1,7 +1,15 @@
 
+//Write a Java program that reads a number in inches and converts it to meters.
+import java.util.Scanner;
+
 public class ConvertInchestoMeters {
 
     public static void main(String[] args) {
-        System.out.println(args[0]);
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println("inches to meter converter.");
+        System.out.print("Input a value for inch:- ");
+        double inches = scanner.nextDouble();
+        double res = inches * 0.0254;
+        System.out.println(inches + " inch is " + String.format("%.3f", res) + " meters");
     }
 }
