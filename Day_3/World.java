5c48b391c60a398ceaf6d933c332f7d993dc54cd
Ritamnandy <nandyritam519@gmail.com>
2026-04-30 01:15:09 +0530

    change

---
+++ Day_3/World.java
@@ -0,0 +1,11 @@
+
+public class World {
+
+    private static void add(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
