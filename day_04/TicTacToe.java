4a44d853017a79f796ca1e71eec088de4447c083
Ritamnandy <nandyritam519@gmail.com>
2026-05-03 13:04:43 +0530

    refactor TicTacToe game logic to use String array and improve win/draw checking

--- day_04/TicTacToe.java
+++ day_04/TicTacToe.java
@@ -4,138 +4,192 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    private static void initilizedarray(char[][] arr) {
-        char k = '1';
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                arr[i][j] = k;
-                k++;
-            }
+    private static void initilizedarray(String[] arr) {
+        // int k = 1;
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.toString(i + 1);
+
         }
     }
 
-    // private static char checkWinningStatus(char[][] arr) {
-    //     char var;
-    //     if ((arr[0][0] == 'X') && (arr[0][1] == 'X') && (arr[0][2] == 'X')) {
-    //         var = 'X';
-    //     } else {
-    //         var = 'O';
-    //     }
-    //     if ((arr[1][0] == 'X') && (arr[1][1] == 'X') && (arr[1][2] == 'X')) {
-    //         var = 'X';
-    //     } else {
-    //         var = 'O';
-    //     }
-    //     if ((arr[2][0] == 'X') && (arr[2][1] == 'X') && (arr[2][2] == 'X')) {
-    //         var = 'X';
-    //     } else {
-    //         var = 'O';
-    //     }
-    //     if ((arr[0][0] == 'X') && (arr[1][0] == 'X') && (arr[2][0] == 'X')) {
-    //         var = 'X';
-    //     } else {
-    //         var = 'O';
-    //     }
-    //     if ((arr[0][0] == 'X') && (arr[1][0] == 'X') && (arr[2][0] == 'X')) {
-    //         var = 'X';
-    //     } else {
-    //         var = 'O';
-    //     }
-    //     return var;
-    // }
-    private static void fieldstructure(char[][] arr) {
+    private static String checkwinanddraw(String[] arr) {
+        String line;
+        line = (arr[0] + arr[1] + arr[2]).trim();
+        if (line.equals("XXX")) {
+
+            return "X";
+        } else if (line.equals("OOO")) {
+            return "O";
+        }
+
+        line = (arr[3] + arr[4] + arr[5]).trim();
+        if (line.equals("XXX")) {
+
+            return "X";
+        } else if (line.equals("OOO")) {
+            return "O";
+        }
+
+        line = (arr[6] + arr[7] + arr[8]).trim();
+        if (line.equals("XXX")) {
+
+            return "X";
+        } else if (line.equals("OOO")) {
+            return "O";
+        }
+
+        line = (arr[0] + arr[3] + arr[6]).trim();
+        if (line.equals("XXX")) {
+
+            return "X";
+        } else if (line.equals("OOO")) {
+            return "O";
+        }
+
+        line = (arr[1] + arr[4] + arr[7]).trim();
+        if (line.equals("XXX")) {
+
+            return "X";
+        } else if (line.equals("OOO")) {
+            return "O";
+        }
+
+        line = (arr[2] + arr[5] + arr[8]).trim();
+        if (line.equals("XXX")) {
+
+            return "X";
+        } else if (line.equals("OOO")) {
+            return "O";
+        }
+
+        line = (arr[0] + arr[4] + arr[8]).trim();
+        if (line.equals("XXX")) {
+
+            return "X";
+        } else if (line.equals("OOO")) {
+            return "O";
+        }
+
+        line = (arr[2] + arr[4] + arr[6]).trim();
+        if (line.equals("XXX")) {
+
+            return "X";
+        } else if (line.equals("OOO")) {
+            return "O";
+        }
+        boolean isDraw = false;
+        for (int i = 0; i < arr.length; i++) {
+            isDraw = arr[i].equals(Integer.toString(i + 1));
+            if (isDraw) {
+                break;
+            }
+        }
+        if (!isDraw) {
+            return "draw";
+        }
+        return null;
+    }
+
+    private static void fieldstructure(String[] arr) {
         System.out.print("-------------------------\n");
         System.out.println("|\t" + "|\t" + "|\t" + "|");
-        System.out.println("    " + arr[0][0] + "    " + "   " + arr[0][1] + "    " + "   " + arr[0][2] + "    ");
+        System.out.println("    " + arr[0] + "    " + "   " + arr[1] + "    " + "   " + arr[2] + "    ");
         System.out.println("|\t" + "|\t" + "|\t" + "|");
         System.out.print("-------------------------\n");
         System.out.println("|\t" + "|\t" + "|\t" + "|");
-        System.out.println("    " + arr[1][0] + "    " + "   " + arr[1][1] + "    " + "   " + arr[1][2] + "    ");
+        System.out.println("    " + arr[3] + "    " + "   " + arr[4] + "    " + "   " + arr[5] + "    ");
         System.out.println("|\t" + "|\t" + "|\t" + "|");
         System.out.print("-------------------------\n");
         System.out.println("|\t" + "|\t" + "|\t" + "|");
-        System.out.println("    " + arr[2][0] + "    " + "   " + arr[2][1] + "    " + "   " + arr[2][2] + "    ");
+        System.out.println("    " + arr[6] + "    " + "   " + arr[7] + "    " + "   " + arr[8] + "    ");
         System.out.println("|\t" + "|\t" + "|\t" + "|");
         System.out.print("-------------------------\n");
     }
 
     public static void main(String[] args) {
-        char[][] arr = new char[3][3];
-        char move = 'X';
-        boolean isWin = true;
         Scanner scanner = new Scanner(System.in);
+        String[] arr = new String[9];
+        String move = "X", isWin = null;
+        int choice;
         TicTacToe.initilizedarray(arr);
 
-        while (isWin) {
+        while (isWin == null) {
             System.out.println("----- Play Tic Tac Toe -----\n");
             System.out.println("");
             TicTacToe.fieldstructure(arr);
-            System.out.print("Enter a slot number to place " + move + " in:- ");
-            int choice = scanner.nextInt();
-            if (choice == 0) {
-                break;
+            System.out.print("Enter a slot number to place \"" + move + "\" in:- ");
+            choice = scanner.nextInt();
+            if (choice == 0 || choice >= 10) {
+                System.out.println("Invalid choice.... ");
+                continue;
             }
             switch (choice) {
                 case 1 -> {
-                    arr[0][0] = move;
+                    arr[0] = move;
                     TicTacToe.fieldstructure(arr);
-                    move = (move == 'X') ? 'O' : 'X';
+                    move = (move.equals("X")) ? "O" : "X";
                 }
 
                 case 2 -> {
-                    arr[0][1] = move;
+                    arr[1] = move;
                     TicTacToe.fieldstructure(arr);
-                    move = (move == 'X') ? 'O' : 'X';
+                    move = (move.equals("X")) ? "O" : "X";
 
                 }
 
                 case 3 -> {
-                    arr[0][2] = move;
+                    arr[2] = move;
                     TicTacToe.fieldstructure(arr);
-                    move = (move == 'X') ? 'O' : 'X';
+                    move = (move.equals("X")) ? "O" : "X";
 
                 }
 
                 case 4 -> {
-                    arr[1][0] = move;
+                    arr[3] = move;
                     TicTacToe.fieldstructure(arr);
-                    move = (move == 'X') ? 'O' : 'X';
+                    move = (move.equals("X")) ? "O" : "X";
 
                 }
 
                 case 5 -> {
-                    arr[1][1] = move;
+                    arr[4] = move;
                     TicTacToe.fieldstructure(arr);
-                    move = (move == 'X') ? 'O' : 'X';
+                    move = (move.equals("X")) ? "O" : "X";
 
                 }
 
                 case 6 -> {
-                    arr[1][2] = move;
+                    arr[5] = move;
                     TicTacToe.fieldstructure(arr);
-                    move = (move == 'X') ? 'O' : 'X';
+                    move = (move.equals("X")) ? "O" : "X";
                 }
 
                 case 7 -> {
-                    arr[2][0] = move;
+                    arr[6] = move;
                     TicTacToe.fieldstructure(arr);
-                    move = (move == 'X') ? 'O' : 'X';
+                    move = (move.equals("X")) ? "O" : "X";
                 }
 
                 case 8 -> {
-                    arr[2][1] = move;
+                    arr[7] = move;
                     TicTacToe.fieldstructure(arr);
-                    move = (move == 'X') ? 'O' : 'X';
+                    move = (move.equals("X")) ? "O" : "X";
                 }
 
                 case 9 -> {
-                    arr[2][2] = move;
+                    arr[8] = move;
                     TicTacToe.fieldstructure(arr);
-                    move = (move == 'X') ? 'O' : 'X';
+                    move = (move.equals("X")) ? "O" : "X";
                 }
                 default ->
                     System.out.println("Invalid choice please try again");
             }
+            isWin = TicTacToe.checkwinanddraw(arr);
+        }
+        if (isWin.trim().equals("draw")) {
+            System.out.println("It's a draw! Thanks for playing.....");
+        } else {
+            System.out.println("Congratulations! " + isWin + "'s have won! Thanks for playing...");
         }
     }
 }
