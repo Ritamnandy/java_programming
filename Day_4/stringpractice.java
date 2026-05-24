9bac42ad00f4a911b04da3090ab6f7f351f49b38
Ritamnandy <nandyritam519@gmail.com>
2026-05-24 16:16:42 +0000

    string practice

---
+++ Day_4/stringpractice.java
@@ -0,0 +1,18 @@
+
+// import  java.util.Scanner;
+public class stringpractice {
+
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Enter a string:- ");
+        // String str = scanner.nextLine();
+        // System.out.println(str);
+        // System.out.println(str.length());
+
+        String str = "Hello World";
+        String str1 = "Hello world";
+        // System.out.println(str.charAt(3));
+        // System.out.println(str.length());
+        System.out.println(str.equalsIgnoreCase(str1));
+    }
+}
