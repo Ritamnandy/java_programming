93a94056a4ecfdd23927b2562be2134393433087
Ritamnandy <nandyritam519@gmail.com>
2026-05-27 19:46:23 +0000

    Comparison of two string

---
+++ Day_4/Comparison.java
@@ -0,0 +1,24 @@
+
+//Write a Java program to compare two strings lexicographically.
+import java.util.Scanner;
+
+public class Comparison {
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println("Compare two strings lexicographically");
+        System.out.print("Enter the first string:- ");
+        String str1 = scanner.nextLine();
+        System.out.print("Enter the second string:- ");
+        String str2 = scanner.nextLine();
+        int result = str1.compareTo(str2);
+        if (result == 0) {
+            System.out.println("\"" + str1 + "\" and \"" + str2 + "\" are equal.");
+        } else if (result > 0) {
+            System.out.println("\"" + str1 + "\" is greater than \"" + str2 + "\".");
+        } else {
+            System.out.println("\"" + str1 + "\" is less than \"" + str2 + "\".");
+        }
+
+    }
+}
