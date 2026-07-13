db50c307f7857137e1c80960cd1fa7c4afa22959
Ritamnandy <nandyritam519@gmail.com>
2026-07-13 18:48:59 +0530

    feat: implement Convert class for string encryption and decryption

---
+++ assign/Convert.java
@@ -0,0 +1,15 @@
+
+// Write a program in Java to make a character encrypted and decrypted again
+import java.util.Scanner;
+
+public class Convert {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string:- ");
+        String str = scanner.nextLine();
+        System.out.println("String in lower case:- " + str.toLowerCase());
+        // scanner.close();
+
+    }
+}
